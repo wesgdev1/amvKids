e14cffadc8f5fff4d4026b89a76088bc6d9abded
Welinton Suarez <wesgdeveloper@gmail.com>
2025-04-21 15:37:50 -0500

    feat: Refactor Paginator component; streamline pagination logic and improve readability

--- src/components/paginator/Paginator.jsx
+++ src/components/paginator/Paginator.jsx
@@ -6,50 +6,45 @@ import {
 } from "./StyledComponents";
 
 export const Paginator = ({ byPage, currentPage, setCurrentPage, total }) => {
-  const pageNumbers = [];
-
-  for (let i = 1; i <= Math.ceil(total / byPage); i++) {
-    pageNumbers.push(i);
-  }
+  const totalPages = Math.ceil(total / byPage);
+  const remainingPages = totalPages - currentPage;
 
   const onPreviousPage = () => {
-    setCurrentPage(currentPage - 1);
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
   };
 
   const onNextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
-
-  const onSpecificPage = (specific) => {
-    setCurrentPage(specific);
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
   };
 
   return (
-    <PaginationStyle className="justify-content-center">
-      <PagPrevStyle
-        className={currentPage === 1 ? "disabled" : ""}
-        onClick={onPreviousPage}
-      >
-        <i className="bi bi-arrow-left p-1"></i>
-        Anterior
-      </PagPrevStyle>
-      {pageNumbers.map((noPage) => (
-        <PagItemStyle
-          key={noPage}
-          active={noPage === currentPage ? true : false}
-          onClick={() => onSpecificPage(noPage)}
+    <div className="flex flex-col items-center pb-3">
+      <PaginationStyle className="justify-content-center">
+        <PagPrevStyle
+          className={currentPage === 1 ? "disabled" : ""}
+          onClick={onPreviousPage}
         >
-          {noPage}
-        </PagItemStyle>
-      ))}
-
-      <PagNextStyle
-        className={currentPage >= pageNumbers.length ? "disabled" : ""}
-        onClick={onNextPage}
-      >
-        Siguiente
-        <i className="bi bi-arrow-right p-1"></i>
-      </PagNextStyle>
-    </PaginationStyle>
+          <i className="bi bi-arrow-left p-1"></i>
+          Anterior
+        </PagPrevStyle>
+        <PagItemStyle active>{currentPage}</PagItemStyle>
+        <PagNextStyle
+          className={currentPage >= totalPages ? "disabled" : ""}
+          onClick={onNextPage}
+        >
+          Siguiente
+          <i className="bi bi-arrow-right p-1"></i>
+        </PagNextStyle>
+      </PaginationStyle>
+      {remainingPages > 0 && (
+        <span className="ms-2">
+          {remainingPages} página{remainingPages > 1 ? "s" : ""} más
+        </span>
+      )}
+    </div>
   );
 };
