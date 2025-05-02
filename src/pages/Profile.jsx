8c0411c0f423d2f5025f8236d19145c6e7fef221
Welinton Suarez <wesgdeveloper@gmail.com>
2025-05-02 12:11:53 -0500

    fix: Remove unnecessary margin from Profile component; improve layout consistency

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { SideBar } from "../components/sidebar/SideBar";
 export const Profile = () => {
   return (
     <div className="pt-4  ">
-      <div className="flex flex-col items-center md:flex-row md:items-start gap-4 px-2  pt-4 mx-5 ">
+      <div className="flex flex-col items-center md:flex-row md:items-start gap-4 px-2  pt-4  ">
         {/* <SideBar /> */}
         <div className=" w-full ">
           <ProfileRoutes />
