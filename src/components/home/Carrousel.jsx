932c7d40659264ccc1803d2457d74d412bfc1236
Welinton Suarez <wesgdeveloper@gmail.com>
2026-04-25 07:52:15 -0500

    feat: Add new item to Carrousel with product image and action button

--- src/components/home/Carrousel.jsx
+++ src/components/home/Carrousel.jsx
@@ -15,6 +15,15 @@ export const Carrousel = () => {
       <Carousel.Item>
         <img
           className="d-block w-100 "
+          src="https://res.cloudinary.com/dppqkypts/image/upload/v1777121478/222PON_EL_LOGO_202604221718_n8zzlj.png"
+          alt="First slide"
+        />
+        {/* Boton de ver productos */}
+        <ButtonAction onClick={handleClick}>Ver productos</ButtonAction>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 "
           src="https://res.cloudinary.com/dppqkypts/image/upload/v1759940088/Copia_de_Dise%C3%B1o_sin_t%C3%ADtulo_2_notzcr.png"
           alt="First slide"
         />
